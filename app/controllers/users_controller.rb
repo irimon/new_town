7970c64537baf07e01c230bc1bd432a4731d1756
irimon <ilanrimon@yahoo.com>
2014-01-27 15:20:51 -0800

    heroku migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   		  		   else
 					   @user.add_interest(int,'no')
   		  		   end
-  		    	#puts params[:interests][:id]  	
+  		    	#puts params[:interests][:id]  j	
   			end	
   		end
   			
